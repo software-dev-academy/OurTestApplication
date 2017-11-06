55c8372b1c6ed0b8472b9539f817e2a4bf4bf22c
Philipp Haller <hallerp@gmail.com>
2017-11-06 13:17:30 +0100

    Add main class of test application
    - Also creates a new package `com.sda`.
    - Main class prints an important message.

---
+++ src/com/sda/OurTestApplication.java
@@ -0,0 +1,10 @@
+package com.sda;
+
+public class OurTestApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Application starting...");
+	}
+
+}
