b542081e38fb9a712e4409151d4777dbf9b97450
Philipp Haller <hallerp@gmail.com>
2017-11-06 13:29:28 +0100

    Minor clean-up

--- src/com/sda/OurTestApplication.java
+++ src/com/sda/OurTestApplication.java
@@ -3,7 +3,6 @@ package com.sda;
 public class OurTestApplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Application starting...");
 	}
 
