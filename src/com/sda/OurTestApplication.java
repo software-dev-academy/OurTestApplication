fd267e37c2d62d92ee81b45e8085136183e249fd
Philipp Haller <hallerp@gmail.com>
2017-11-22 10:52:25 +0100

    Improve splash screen
    Adds team name to the welcome message.

--- src/com/sda/OurTestApplication.java
+++ src/com/sda/OurTestApplication.java
@@ -4,6 +4,7 @@ public class OurTestApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Application starting...");
+		System.out.println("Developed by team Not My Head");
 	}
 
 }
